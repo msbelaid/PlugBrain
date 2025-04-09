86ae2b547a10624584f7856438a0a770a41511d0
Mohammed-Said Belaid <said.belaid@univ-usto.dz>
2025-04-09 10:50:56 +0100

    Add Ktlint, upgrade all dependencies

--- app/src/main/java/app/matholck/android/MainActivity.kt
+++ app/src/main/java/app/matholck/android/MainActivity.kt
@@ -14,34 +14,37 @@ import androidx.compose.ui.tooling.preview.Preview
 import app.matholck.android.ui.theme.MathlockAppTheme
 
 class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            MathlockAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    enableEdgeToEdge()
+    setContent {
+      MathlockAppTheme {
+        Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+          Greeting(
+            name = "Android",
+            modifier = Modifier.padding(innerPadding),
+          )
         }
+      }
     }
+  }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Greeting(
+  name: String,
+  modifier: Modifier = Modifier,
+) {
+  Text(
+    text = "Hello $name!",
+    modifier = modifier,
+  )
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    MathlockAppTheme {
-        Greeting("Android")
-    }
+  MathlockAppTheme {
+    Greeting("Android")
+  }
 }
