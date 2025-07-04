ecda17a3aefba9c2d7b18fd58ccb035d33971fcd
Mohammed-Said Belaid <said.belaid@univ-usto.dz>
2025-07-05 00:08:19 +0100

    Release on new tag version + write permission

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "app.plugbrain.android"
         minSdk = 28
         targetSdk = 35
-        versionCode = 3
-        versionName = "1.1"
+        versionCode = 12
+        versionName = "1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
