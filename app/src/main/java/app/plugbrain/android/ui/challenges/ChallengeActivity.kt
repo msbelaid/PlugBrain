aa66e85f5a812878fdf53a2117af89c802b0cf21
Mohammed-Said Belaid <said.belaid@univ-usto.dz>
2025-07-17 23:22:38 +0100

    Fix state value

--- app/src/main/java/app/plugbrain/android/ui/challenges/ChallengeActivity.kt
+++ app/src/main/java/app/plugbrain/android/ui/challenges/ChallengeActivity.kt
@@ -1,24 +1,15 @@
 package app.plugbrain.android.ui.challenges
 
-import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Modifier
-import app.plugbrain.android.BuildConfig
-import app.plugbrain.android.ui.challenges.compose.ArithChallengeScreen
-import app.plugbrain.android.ui.challenges.compose.ChallengeTopBar
 import app.plugbrain.android.ui.challenges.compose.CountChallengeScreen
 import app.plugbrain.android.ui.challenges.presentation.ArithChallengeViewModel
 import app.plugbrain.android.ui.theme.MathlockAppTheme
-import app.plugbrain.android.ui.timer.TimerActivity
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class ChallengeActivity : ComponentActivity() {
@@ -34,13 +25,12 @@ class ChallengeActivity : ComponentActivity() {
     enableEdgeToEdge()
     setContent {
       MathlockAppTheme {
-        val challenge by challengeViewModel.mathChallenge.collectAsState()
-        val countValue = challengeViewModel.countChallenge
+        val countValue by challengeViewModel.countChallenge.collectAsState()
 
         CountChallengeScreen(
           countValue,
         ) { filledValue ->
-          if (countValue == filledValue)  {
+          if (countValue == filledValue) {
             challengeViewModel.unblockApps()
             finishAffinity()
           } else {
