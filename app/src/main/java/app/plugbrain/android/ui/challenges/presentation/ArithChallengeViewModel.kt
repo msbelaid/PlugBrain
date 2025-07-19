defd8db09cd9a252782ecaa7bbf2f7d140b65b72
Mohammed-Said Belaid <said.belaid@univ-usto.dz>
2025-07-19 08:14:56 +0100

    Fix blinking challenge (#25)
    #4

--- app/src/main/java/app/plugbrain/android/ui/challenges/presentation/ArithChallengeViewModel.kt
+++ app/src/main/java/app/plugbrain/android/ui/challenges/presentation/ArithChallengeViewModel.kt
@@ -5,13 +5,10 @@ import androidx.lifecycle.viewModelScope
 import app.plugbrain.android.datastore.DataStoreManager
 import app.plugbrain.android.repository.MathChallengeRepository
 import app.plugbrain.android.repository.model.MathChallenge
-import app.plugbrain.android.service.TAG
-import kotlin.time.Duration.Companion.milliseconds
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.launch
-import timber.log.Timber
 
 class ArithChallengeViewModel(
   private val dataStoreManager: DataStoreManager,
@@ -22,21 +19,8 @@ class ArithChallengeViewModel(
 
   fun generateChallenge() {
     viewModelScope.launch {
-      decreaseDifficulty()
       _mathChallenge.value = mathChallengeRepository.generateProgressiveChallenge().first()
       dataStoreManager.updateLastChallengeTime(System.currentTimeMillis())
-    }
-  }
-
-  suspend fun decreaseDifficulty() {
-    val it = dataStoreManager.getTimeStats().first()
-    val durationSinceLastChallenge = (
-      System.currentTimeMillis() - (it.lastChallengeGenerateTime ?: 0)
-      ).milliseconds.inWholeMinutes.toInt()
-    Timber.tag(TAG).e("Duration since last challenge:%s", durationSinceLastChallenge.toString())
-    if (durationSinceLastChallenge >= it.blockInterval * 2) {
-      val nbDecLevels = durationSinceLastChallenge / (it.blockInterval * 2)
-      dataStoreManager.decrementProgressiveDifficulty(nbDecLevels)
     }
   }
 
