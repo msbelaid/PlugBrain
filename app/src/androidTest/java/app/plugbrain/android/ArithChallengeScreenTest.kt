d1b8ec1b9b07fa979accce34ca32ae45c411669a
Mohammed-Said Belaid <said.belaid@univ-usto.dz>
2025-07-27 15:20:53 +0100

    Create UI tests

---
+++ app/src/androidTest/java/app/plugbrain/android/ArithChallengeScreenTest.kt
@@ -0,0 +1,47 @@
+package app.plugbrain.android
+
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.*
+import app.plugbrain.android.repository.model.MathChallenge
+import app.plugbrain.android.repository.model.Operator
+import app.plugbrain.android.ui.challenges.compose.ArithChallengeScreen
+import kotlinx.coroutines.delay
+import org.junit.Rule
+import org.junit.Test
+
+class ArithChallengeScreenTest {
+
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+    @Test
+    fun testArithChallengeScreenDisplaysOperandsAndAcceptsInput() {
+        var result: Int? = null
+
+        val challenge = MathChallenge(
+            num1 = 7,
+            num2 = 3,
+            operator = Operator.ADDITION
+        )
+
+        composeTestRule.setContent {
+            ArithChallengeScreen (
+                mathChallenge = challenge,
+                checkAnswer = { result = it }
+            )
+        }
+
+        composeTestRule.onNodeWithText("7").assertIsDisplayed()
+        composeTestRule.onNodeWithText("+").assertIsDisplayed()
+        composeTestRule.onNodeWithText("3").assertIsDisplayed()
+
+        composeTestRule.onNode(hasSetTextAction()).performTextInput("10")
+
+        composeTestRule.onNodeWithText(">").performClick()
+        assert(result == 10)
+    }
+    @Test
+    fun testArithChallengeScreenDisplaysOperandsAndAcceptsInput2() {
+
+    }
+}
