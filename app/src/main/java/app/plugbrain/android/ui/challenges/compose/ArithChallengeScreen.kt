c1aba8237d850e95028668a8efdc502457f93c87
Feriel080 <aourferiel22@outlook.com>
2025-08-16 12:08:10 +0100

    removing comments

--- app/src/main/java/app/plugbrain/android/ui/challenges/compose/ArithChallengeScreen.kt
+++ app/src/main/java/app/plugbrain/android/ui/challenges/compose/ArithChallengeScreen.kt
@@ -30,11 +30,13 @@ import androidx.compose.ui.draw.scale
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import app.plugbrain.android.R
 import app.plugbrain.android.repository.model.MathChallenge
 import app.plugbrain.android.repository.model.Operator
 import app.plugbrain.android.ui.isPortrait
@@ -45,7 +47,6 @@ fun ArithChallengeScreen(
   mathChallenge: MathChallenge,
   checkAnswer: (Int) -> Unit,
 ) {
-//   Tracking if the message animation is done
   var messageAnimationDone by remember { mutableStateOf(false) }
   Column(
     horizontalAlignment = Alignment.CenterHorizontally,
@@ -100,7 +101,7 @@ private fun BlockAppsMessage(onAnimationDone: () -> Unit) {
   }
 
   Text(
-    text = "Time's up!\nConnect your brain to unlock the app!",
+    text = stringResource(R.string.blocking_message),
     style = MaterialTheme.typography.titleLarge.copy(letterSpacing = animatedLetterSpacingValue.sp),
     textAlign = TextAlign.Center,
     modifier = Modifier.alpha(animatedAlpha),
