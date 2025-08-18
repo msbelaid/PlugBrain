9970c177622f09f3de377cd1dce9744144aa7230
Mohammed-Said Belaid <said.belaid@univ-usto.dz>
2025-08-18 01:14:09 +0100

    Add Addition Zero level

--- app/src/main/java/app/plugbrain/android/challenges/addition/AdditionUnderFiveChallenge.kt
+++ app/src/main/java/app/plugbrain/android/challenges/addition/AdditionUnderFiveChallenge.kt
@@ -1,7 +1,7 @@
 package app.plugbrain.android.challenges.addition
 
 class AdditionUnderFiveChallenge : AdditionChallenge {
-  override val operand1: Int = (1..5).random()
-  override val operand2: Int = (1..5).random()
+  override val operand1: Int = (2..5).random()
+  override val operand2: Int = (2..5).random()
   override val difficultyLevel: Int = 1
 }
