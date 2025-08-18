f49b52e1e86766d73800043a8295103de5d9585e
Mohammed-Said Belaid <said.belaid@univ-usto.dz>
2025-08-18 01:15:49 +0100

    remove 10 from mult challenges

--- app/src/main/java/app/plugbrain/android/challenges/multiplication/MultiplicationByElevenChallenge.kt
+++ app/src/main/java/app/plugbrain/android/challenges/multiplication/MultiplicationByElevenChallenge.kt
@@ -2,6 +2,6 @@ package app.plugbrain.android.challenges.multiplication
 
 class MultiplicationByElevenChallenge : MultiplicationChallenge {
   override val operand1: Int = 11
-  override val operand2: Int = (2..10).random()
+  override val operand2: Int = (2..9).random()
   override val difficultyLevel: Int = 3
 }
