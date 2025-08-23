c35e5f2a0e24070010002c634805c4802428cae0
ioit-aaa <211367176+ioit-aaa@users.noreply.github.com>
2025-08-23 18:29:10 +0800

    fix: update versionCode to 14 and versionName to 1.4.1
    Fixes #35
    
    Signed-off-by: ioit-aaa <211367176+ioit-aaa@users.noreply.github.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "app.plugbrain.android"
         minSdk = 28
         targetSdk = 35
-        versionCode = 13
-        versionName = "1.3"
+        versionCode = 14
+        versionName = "1.4.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
