919da93789e2e567752312e09ba55c268eb9b38e
Mohammed-Said Belaid <said.belaid@univ-usto.dz>
2025-08-30 23:31:41 +0100

    update difficulties!

--- app/src/main/java/app/plugbrain/android/challenges/addition/AdditionFourDigitsChallenge.kt
+++ app/src/main/java/app/plugbrain/android/challenges/addition/AdditionFourDigitsChallenge.kt
@@ -3,5 +3,5 @@ package app.plugbrain.android.challenges.addition
 class AdditionFourDigitsChallenge : AdditionChallenge {
   override val operand1: Int = (1000..10000).random()
   override val operand2: Int = (1000..10000).random()
-  override val difficultyLevel: Int = 7
+  override val difficultyLevel: Int = 8
 }
--- app/src/main/java/app/plugbrain/android/challenges/substraction/SubtractOneDigitChallenge.kt
+++ app/src/main/java/app/plugbrain/android/challenges/substraction/SubtractOneDigitChallenge.kt
@@ -1,7 +1,7 @@
 package app.plugbrain.android.challenges.substraction
 
 class SubtractOneDigitChallenge : SubstractionChallenge {
-  override val operand2: Int = (1..9).random()
+  override val operand2: Int = (3..9).random()
   override val operand1: Int = (operand2..9).random()
   override val difficultyLevel: Int = 3
 }
