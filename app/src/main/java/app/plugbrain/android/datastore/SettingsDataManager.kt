a9c43dee851556cf2f85448d017a2c4f7db301b3
Mohammed-Said Belaid <said.belaid@univ-usto.dz>
2025-08-30 15:20:29 +0100

    Update the UserSetting data manager

--- app/src/main/java/app/plugbrain/android/datastore/SettingsDataManager.kt
+++ app/src/main/java/app/plugbrain/android/datastore/SettingsDataManager.kt
@@ -2,26 +2,30 @@ package app.plugbrain.android.datastore
 
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.booleanPreferencesKey
 import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.core.intPreferencesKey
 import androidx.datastore.preferences.core.stringSetPreferencesKey
-import app.plugbrain.android.datastore.model.Settings
+import app.plugbrain.android.datastore.model.UserSettings
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 
 val BLOCKED_APPS_KEY = stringSetPreferencesKey("blocked_apps")
 val BLOCK_INTERVAL_KEY = intPreferencesKey("block_interval")
 val MINIMAL_DIFFICULTY_KEY = intPreferencesKey("minimal_difficulty")
+val PROGRESSIVE_DIFFICULTY_KEY = intPreferencesKey("progressive_difficulty")
+val BLOCK_APPS_TOGGLE_KEY = booleanPreferencesKey("block_apps_toggle")
 
-private const val DEFAULT_BLOCK_INTERVAL = 15
-private const val DEFAULT_MINIMAL_DIFFICULTY = 1
+const val DEFAULT_BLOCK_INTERVAL = 5
+const val DEFAULT_MINIMAL_DIFFICULTY = 1
 
-fun DataStore<Preferences>.getUserSettings(): Flow<Settings> =
+fun DataStore<Preferences>.getUserSettings(): Flow<UserSettings> =
   data.map {
-    Settings(
+    UserSettings(
       blockInterval = it[BLOCK_INTERVAL_KEY] ?: DEFAULT_BLOCK_INTERVAL,
       distractiveApps = it[BLOCKED_APPS_KEY] ?: emptySet(),
       selectedMinimalDifficulty = it[MINIMAL_DIFFICULTY_KEY] ?: DEFAULT_MINIMAL_DIFFICULTY,
+      currentDifficultyLevel = it[PROGRESSIVE_DIFFICULTY_KEY] ?: it[MINIMAL_DIFFICULTY_KEY] ?: DEFAULT_MINIMAL_DIFFICULTY,
     )
   }
 
@@ -42,5 +46,40 @@ suspend fun DataStore<Preferences>.addDistractiveApp(packageName: String) {
 suspend fun DataStore<Preferences>.removeDistractiveApp(packageName: String) {
   edit { preferences ->
     preferences[BLOCKED_APPS_KEY] = preferences[BLOCKED_APPS_KEY]?.minus(packageName) ?: emptySet()
+  }
+}
+
+// TODO Extract in a different file
+fun DataStore<Preferences>.getBlockAppsToggle() = data
+  .map { preferences ->
+    preferences[BLOCK_APPS_TOGGLE_KEY]
+  }
+
+suspend fun DataStore<Preferences>.updateBlockAppsToggle(isBlocked: Boolean) {
+  edit { prefs ->
+    prefs[BLOCK_APPS_TOGGLE_KEY] = isBlocked
+  }
+}
+
+fun DataStore<Preferences>.getProgressiveDifficulty() = data
+  .map { preferences ->
+    preferences[PROGRESSIVE_DIFFICULTY_KEY] ?: preferences[MINIMAL_DIFFICULTY_KEY] ?: DEFAULT_MINIMAL_DIFFICULTY
+  }
+
+suspend fun DataStore<Preferences>.decrementProgressiveDifficulty(steps: Int = 1) {
+  edit { prefs ->
+    val minDifficulty = prefs[MINIMAL_DIFFICULTY_KEY] ?: DEFAULT_MINIMAL_DIFFICULTY
+    val current = prefs[PROGRESSIVE_DIFFICULTY_KEY] ?: minDifficulty
+
+    val newValue = (current - steps).coerceAtLeast(minDifficulty)
+    prefs[PROGRESSIVE_DIFFICULTY_KEY] = newValue
+  }
+}
+
+suspend fun DataStore<Preferences>.incrementProgressiveDifficulty(maxDifficulty: Int) {
+  edit { prefs ->
+    val current = prefs[PROGRESSIVE_DIFFICULTY_KEY] ?: 0
+    val newValue = (current + 1).coerceAtMost(maxDifficulty)
+    prefs[PROGRESSIVE_DIFFICULTY_KEY] = newValue
   }
 }
