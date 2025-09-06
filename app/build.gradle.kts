831a3fab6fb446008af8815fd6cf3171caf049a5
Mohammed-Said Belaid <said.belaid@univ-usto.dz>
2025-09-06 07:49:43 +0100

    Increment version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "app.plugbrain.android"
         minSdk = 28
         targetSdk = 35
-        versionCode = 14
-        versionName = "1.4.1"
+        versionCode = 151
+        versionName = "1.5.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
