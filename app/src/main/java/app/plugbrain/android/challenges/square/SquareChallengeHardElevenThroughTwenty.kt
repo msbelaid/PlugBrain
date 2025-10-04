c799bd258eadf019ad6ced23326676f7ead95f6b
Zephyr-Pixilated <customerinfo.gyoc@gmail.com>
2025-10-04 10:55:08 -0500

    Updated inherited classes

--- app/src/main/java/app/plugbrain/android/challenges/square/SquareChallengeHardElevenThroughTwenty.kt
+++ app/src/main/java/app/plugbrain/android/challenges/square/SquareChallengeHardElevenThroughTwenty.kt
@@ -1,29 +1,19 @@
-package app.plugbrain.android.challenges
+package app.plugbrain.android.challenges.square
+import app.plugbrain.android.challenges.SquareChallenge
 
-class SquareChallenge : NumericalChallenge {
+class SquareChallengeHardElevenThroughTwenty : SquareChallenge() {
   //TODO("Add the challenge fields")
-   val operand1: Int = (2..5).random()
-   val operand2: Int = (6..10).random()
-   val operand3: Int = (2..9).random()
+  override val operand: Int = (11..20).random()
 
   override fun checkAnswer(response: Int): Boolean {
     TODO("Check the response here!")
-//    return operand1 * operand1 == response
-    if(operand1 * operand1 == response)
-    {
-      return true
-    }
-    else
-    {
-      return false
-    }
+    return operand * operand == response
   }
 
-  override val difficultyLevel: Int
-    get() = TODO("Put difficulty level here")
+  override val difficultyLevel: Int = 3
   //  override val difficultyLevel: Int = 1, 2, or 3
   override fun string(): String {
     TODO("How the challenge should be displayed")
-    return operand1.toString() + "² = ?"
+    return operand.toString() + "² = ?"
   }
 }
