485932fe930563436a778e0bd38c4024a43917e5
Samuel Novotný <141497538+samuelnvtn@users.noreply.github.com>
2026-02-26 22:27:00 +0100

    new color system (#99) (#106)

--- app/src/main/java/app/plugbrain/android/ui/theme/Theme.kt
+++ app/src/main/java/app/plugbrain/android/ui/theme/Theme.kt
@@ -12,16 +12,26 @@ import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme =
   darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80,
+    primary = Primary500,
+    secondary = Primary400,
+    surface = Neutral700,
+    onSurface = Primary100,
+    onPrimary = White,
+    onSecondary = Primary100,
+    background = Neutral900,
+    error = Error500,
   )
 
 private val LightColorScheme =
   lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40,
+    primary = Primary500,
+    secondary = Primary400,
+    surface = White,
+    onSurface = Primary900,
+    onPrimary = White,
+    onSecondary = Primary900,
+    background = White,
+    error = Error500,
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
     surface = Color(0xFFFFFBFE),
