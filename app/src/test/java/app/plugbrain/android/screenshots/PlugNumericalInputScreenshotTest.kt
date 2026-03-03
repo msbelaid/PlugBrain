a4bf2ab5733b87f6238fa04eef247f3f3521639e
Mohammed-Said Belaid <said.belaid@univ-usto.dz>
2026-03-03 13:24:21 +0100

    add screenshot tests

---
+++ app/src/test/java/app/plugbrain/android/screenshots/PlugNumericalInputScreenshotTest.kt
@@ -0,0 +1,103 @@
+package app.plugbrain.android.screenshots
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.focusable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.unit.dp
+import app.cash.paparazzi.DeviceConfig
+import app.cash.paparazzi.Paparazzi
+import app.plugbrain.android.ui.designsystem.components.button.PlugNumericalInput
+import app.plugbrain.android.ui.theme.MathlockAppTheme
+import org.junit.Rule
+import org.junit.Test
+
+class PlugNumericalInputScreenshotTest {
+    @get:Rule
+    val paparazzi =
+        Paparazzi(
+            deviceConfig = DeviceConfig.PIXEL_5,
+        )
+
+    @Test
+    fun plugNumericalInputTests() {
+        paparazzi.snapshot {
+            numericalInputTest()
+        }
+    }
+
+    @Test
+    fun plugNumericalInputDarkModeTests() {
+        paparazzi.snapshot {
+            numericalInputTest(darkTheme = true)
+        }
+    }
+
+    @Composable
+    private fun numericalInputTest(darkTheme: Boolean = false) {
+        val focusRequester = remember { FocusRequester() }
+
+        LaunchedEffect(Unit) {
+            focusRequester.requestFocus()
+        }
+
+        MathlockAppTheme(dynamicColor = false, darkTheme = darkTheme) {
+            Column(
+                modifier = Modifier.background(MaterialTheme.colorScheme.surface),
+            ) {
+                PlugNumericalInput(
+                    value = "",
+                    onValueChange = {},
+                    onSubmit = {},
+                    placeholder = "Unfocused",
+                    modifier =
+                        Modifier
+                            .padding(8.dp)
+                            .fillMaxWidth(),
+                )
+                PlugNumericalInput(
+                    value = "Focused",
+                    onValueChange = {},
+                    onSubmit = {},
+                    placeholder = "Focused",
+                    modifier =
+                        Modifier
+                            .focusRequester(focusRequester)
+                            .focusable()
+                            .padding(8.dp)
+                            .fillMaxWidth(),
+                )
+                PlugNumericalInput(
+                    value = "Success",
+                    onValueChange = {},
+                    onSubmit = {},
+                    placeholder = "Success",
+                    isSuccess = true,
+                    modifier =
+                        Modifier
+                            .padding(8.dp)
+                            .fillMaxWidth(),
+                )
+                PlugNumericalInput(
+                    value = "Error",
+                    onValueChange = {},
+                    onSubmit = {},
+                    placeholder = "Error",
+                    isError = true,
+                    modifier =
+                        Modifier
+                            .padding(8.dp)
+                            .fillMaxWidth(),
+                )
+            }
+        }
+    }
+}
